f7ef5430c020220ff600c103c3829cd83d8b6073
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-08-14 22:42:19 +0530

    Added Store and Slices

---
+++ src/store/postSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+const postSlice = createSlice({
+    name: "post",
+    initialState,
+    reducers: {}
+})
+
+export const {} = postSlice.actions;
+
+export default postSlice.reducer;
